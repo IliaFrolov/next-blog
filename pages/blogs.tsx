c557b93e1f49858164dc3d96f43e92f2d6918aaf
Frolov <ilia.frolov@intive.com>
2023-03-20 22:09:07 +0200

    Next server side fetch

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,33 +1,42 @@
 import BlogCard from "@/components/BlogCart";
-import { NextPage } from "next";
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import { useEffect, useState } from "react";
 
-interface BlogsProps {}
+interface PostApiResponse {
+  postInfo: PostType[];
+}
+interface BlogsProps {
+  posts: PostType[];
+  error?: string;
+}
 type PostType = {
   title: string;
   slug: string;
   meta: string;
 };
-const Blogs: NextPage = ({}: BlogsProps) => {
-  const [posts, setPost] = useState<PostType[]>([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const res = await fetch("/api/posts").then((data) => data.json());
-        setPost(res.postInfo);
-      } catch (err) {
-        console.error(err);
-      }
+export const getStaticProps: GetStaticProps<BlogsProps> = async () => {
+  try {
+    const { postInfo }: PostApiResponse = await fetch(
+      "http://localhost:3000/api/posts"
+    ).then((data) => data.json());
+    return {
+      props: { posts: postInfo },
     };
-    fetchPosts();
-  }, []);
+  } catch (err) {
+    console.error(err);
+    return {
+      props: { error: `${err}`, posts: [] },
+    };
+  }
+};
 
+const Blogs: NextPage<BlogsProps> = ({ posts, error }) => {
+  if (error) return <>{error}</>;
   if (!posts?.length) return null;
   return (
     <div className="max-w-3xl mx-auto p-5 space-y-5">
       {posts.map(({ title, slug, meta }) => (
-        <BlogCard title={title} description={meta} />
+        <BlogCard key={slug} slug={slug} title={title} description={meta} />
       ))}
     </div>
   );
